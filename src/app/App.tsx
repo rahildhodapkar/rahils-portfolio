3a1eb38bb30ba22c516e09f3379d2974b0b8b42f
Rahil <rahildhodapkar@gmail.com>
2026-03-16 18:39:57 -0400

    Initial styles

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Hero from '../sections/hero/Hero';
+
+function App() {
+  return (
+    <>
+      <header>
+        <Hero />
+      </header>
+    </>
+  );
+}
+
+export default App;
---
+++ src/sections/hero/Hero.tsx
@@ -0,0 +1,5 @@
+import styles from './Hero.module.css';
+
+export default function Hero() {
+    return <h1 className={styles.title}>Rahil Dhodapkar</h1>;
+}
